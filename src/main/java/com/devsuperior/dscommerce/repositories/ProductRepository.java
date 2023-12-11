316075826941638700fe7b4bea664225c51517d0
luciosilva <sandmanlucio@gmail.com>
2023-12-10 22:21:10 -0300

    add get product by id

---
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dscommerce.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
