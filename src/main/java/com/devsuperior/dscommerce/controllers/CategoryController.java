63ba858e5565c6e8ed1ad3c71d8d33816e5259bf
luciosilva <sandmanlucio@gmail.com>
2023-12-22 23:30:15 -0300

    add get categories

---
+++ src/main/java/com/devsuperior/dscommerce/controllers/CategoryController.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.services.CategoryService;
+import com.devsuperior.dscommerce.services.ProductService;
+
+import jakarta.validation.Valid;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
+import com.devsuperior.dscommerce.entities.Product;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> list = service.findAll();
+        return ResponseEntity.ok(list);
+    }
+
+}
